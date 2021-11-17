67738737ef4eafc104033203e30c069a60b6f02b
debangi <debangiofficial@gmail.com>
2021-11-17 21:59:57 +0530

    Key Code Generator

--- KeyCodeGenerator/script.js
+++ KeyCodeGenerator/script.js
@@ -1,7 +1,6 @@
 const keyNameEl = document.querySelector(".keyName");
 const codeNameEl = document.querySelector(".codeName");
 document.addEventListener("keyup", function (e) {
-  console.log(e);
   keyNameEl.textContent = e.key;
   codeNameEl.textContent = e.code;
 });
