2ee632ba1c966e4a126020f42ffc9f1a3711253a
debangi <debangiofficial@gmail.com>
2021-11-29 22:03:17 +0530

    Form Validation

--- FormValidator/script.js
+++ FormValidator/script.js
@@ -3,3 +3,18 @@ const password1El = document.getElementById("password1");
 const password2El = document.getElementById("password2");
 const messageCtn = document.querySelector(".messageContainer");
 const messageEl = document.getElementById("message");
+
+let isValid = false;
+
+function validateForm() {
+  isValid = form.checkValidity();
+  messageEl.textContent = "Please Fill out all fields!";
+  messageEl.style.color = "red";
+  messageCtn.style.borderColor = "red";
+}
+function processFormData(e) {
+  e.preventDefault();
+  validateForm();
+}
+
+form.addEventListener("submit", processFormData);
