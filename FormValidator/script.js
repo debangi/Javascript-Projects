99e38f49c3ec234280dac8756d5cd307161cec21
debangi <debangiofficial@gmail.com>
2021-11-29 21:50:50 +0530

    Form Validation

---
+++ FormValidator/script.js
@@ -0,0 +1,5 @@
+const form = document.getElementById("form");
+const password1El = document.getElementById("password1");
+const password2El = document.getElementById("password2");
+const messageCtn = document.querySelector(".messageContainer");
+const messageEl = document.getElementById("message");
