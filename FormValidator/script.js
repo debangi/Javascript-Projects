5617352d50c8becc1224c252b8d00bdec1fa40b6
debangi <debangiofficial@gmail.com>
2021-11-29 22:08:31 +0530

    Form Validation

--- FormValidator/script.js
+++ FormValidator/script.js
@@ -5,12 +5,27 @@ const messageCtn = document.querySelector(".messageContainer");
 const messageEl = document.getElementById("message");
 
 let isValid = false;
+let passwordsMatch = false;
 
 function validateForm() {
   isValid = form.checkValidity();
-  messageEl.textContent = "Please Fill out all fields!";
-  messageEl.style.color = "red";
-  messageCtn.style.borderColor = "red";
+  if (!isValid) {
+    messageEl.textContent = "Please Fill out all fields!";
+    messageEl.style.color = "red";
+    messageCtn.style.borderColor = "red";
+  }
+  if (password1El.value === password2El.value) {
+    passwordsMatch = true;
+    password1El.style.borderColor = "green";
+    password2El.style.borderColor = "green";
+  } else {
+    passwordsMatch = false;
+    messageEl.textContent = "Make sure passwords match!";
+    messageEl.style.color = "red";
+    messageCtn.style.borderColor = "red";
+    password1El.style.borderColor = "red";
+    password2El.style.borderColor = "red";
+  }
 }
 function processFormData(e) {
   e.preventDefault();
