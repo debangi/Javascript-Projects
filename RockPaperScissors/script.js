210119a63d67006afdbbd6cc4604ceeb97d3981b
debangi <debangiofficial@gmail.com>
2021-11-30 23:47:23 +0530

    Rock Paper Game

--- RockPaperScissors/script.js
+++ RockPaperScissors/script.js
@@ -25,14 +25,68 @@ const choices = {
   lizard: { name: "Lizard", defeats: ["paper", "spock"] },
   spock: { name: "Spock", defeats: ["scissors", "rock"] },
 };
+let computerChoice = "";
 
 function resetSelected() {
   allGameIcons.forEach((icon) => {
     icon.classList.remove("selected");
   });
 }
-function select(playerChoice) {
+function computerRandomChoice() {
+  const computerChoiceNumber = Math.floor(Math.random() * 5) + 1;
+  switch (computerChoiceNumber) {
+    case 1:
+      computerChoice = "rock";
+      break;
+    case 2:
+      computerChoice = "paper";
+      break;
+    case 3:
+      computerChoice = "scissors";
+      break;
+    case 4:
+      computerChoice = "lizard";
+      break;
+    case 5:
+      computerChoice = "spock";
+      break;
+    default:
+      break;
+  }
+}
+function displayComputerChoice() {
+  switch (computerChoice) {
+    case "rock":
+      computerRock.classList.add("selected");
+      computerChoiceEl.textContent = `--- ${computerRock.title}`;
+      break;
+    case "paper":
+      computerPaper.classList.add("selected");
+      computerChoiceEl.textContent = `--- ${computerPaper.title}`;
+      break;
+    case "scissors":
+      computerScissors.classList.add("selected");
+      computerChoiceEl.textContent = `--- ${computerScissors.title}`;
+      break;
+    case "lizard":
+      computerLizard.classList.add("selected");
+      computerChoiceEl.textContent = `--- ${computerLizard.title}`;
+      break;
+    case "spock":
+      computerSpock.classList.add("selected");
+      computerChoiceEl.textContent = `--- ${computerSpock.title}`;
+      break;
+    default:
+      break;
+  }
+}
+function checkResult() {
   resetSelected();
+  computerRandomChoice();
+  displayComputerChoice();
+}
+function select(playerChoice) {
+  checkResult();
   switch (playerChoice) {
     case "rock":
       playerRock.classList.add("selected");
