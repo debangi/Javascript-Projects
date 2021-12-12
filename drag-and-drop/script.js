abec0fa74fc40d07c17395259f8fc854d73333c7
debangi <debangiofficial@gmail.com>
2021-12-12 23:54:23 +0530

    drag and drop

--- drag-and-drop/script.js
+++ drag-and-drop/script.js
@@ -92,13 +92,29 @@ function updateDOM() {
   });
 
   // Run getSavedColumns only once, Update Local Storage
+  updatedOnLoad = true;
+  updateSavedColumns();
 }
 function rebuildArrays() {
   console.log(backlogList.children);
   console.log(progressList.children);
+  backlogListArray = [];
   for (let i = 0; i < backlogList.children.length; i++) {
     backlogListArray.push(backlogList.children[i].textContent);
   }
+  progressListArray = [];
+  for (let i = 0; i < progressList.children.length; i++) {
+    progressListArray.push(progressList.children[i].textContent);
+  }
+  completeListArray = [];
+  for (let i = 0; i < completeList.children.length; i++) {
+    completeListArray.push(completeList.children[i].textContent);
+  }
+  onHoldListArray = [];
+  for (let i = 0; i < onHoldList.children.length; i++) {
+    onHoldListArray.push(onHoldList.children[i].textContent);
+  }
+  updateDOM();
 }
 function drag(e) {
   draggedItem = e.target;
